0da7f12777ab221f63e860d02dd439247808d7ae
grifkies <grifkies@protonmail.com>
2024-01-30 22:36:31 -0700

    Working on the cool stuff

---
+++ scripts/exec_node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ec2-user/app/
+forever server.js
--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/app/
+
 systemctl reset-failed
-systemd-run --unit=resume-node forever server.js
+systemd-run --unit=resume-node /home/ec2-user/app/scripts/exec_node.sh
 systemctl stop httpd
 systemctl start httpd
