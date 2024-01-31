5a353d232c1479d425d033aba5209f20e682d634
grifkies <grifkies@protonmail.com>
2024-01-30 22:42:08 -0700

    almost there

--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 systemctl reset-failed
-systemd-run --unit=resume-node /home/ec2-user/app/scripts/exec_node.sh
+systemd-run --unit=resume-node /bin/bash /home/ec2-user/app/scripts/exec_node.sh
 systemctl stop httpd
 systemctl start httpd
